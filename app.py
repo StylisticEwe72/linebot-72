45aafe7173d56f687736b2e079709f4d78f950bb
StylisticEwe72 <u66172115a@gmail.com>
2022-02-17 18:21:22 +0800

    優化猜拳

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def handle_message(event):
 
     if '我出' in line_text:
         mora_list = ['剪刀', '石頭', '布']
+        a = random.choice(mora_list)
         if '剪刀' in line_text:
-            a = random.choice(mora_list)
             mora_player = '剪刀'
             if a == '石頭':
                 mora_end = '我贏了'
@@ -98,9 +98,8 @@ def handle_message(event):
             else:
                 mora_end = '沒輸沒贏'
             mora_txt = f'你出:{mora_player}\n我出:{a}\n\n結果:{mora_end}'
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora_txt))
+
         elif '石頭' in line_text:
-            a = random.choice(mora_list)
             mora_player = '石頭'
             if a == '剪刀':
                 mora_end = '你贏了'
@@ -109,9 +108,8 @@ def handle_message(event):
             else:
                 mora_end = '沒輸沒贏'
             mora_txt = f'你出:{mora_player}\n我出:{a}\n\n結果:{mora_end}'
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora_txt))
+
         elif '布' in line_text:
-            a = random.choice(mora_list)
             mora_player = '布'
             if a == '石頭':
                 mora_end = '你贏了'
@@ -120,7 +118,7 @@ def handle_message(event):
             else:
                 mora_end = '沒輸沒贏'
             mora_txt = f'你出:{mora_player}\n我出:{a}\n\n結果:{mora_end}'
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora_txt))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=mora_txt))
 
     if '不要問選一個 ' in line_text:
         event.message.text = event.message.text.replace('不要問選一個 ', '')
@@ -226,8 +224,6 @@ def handle_message(event):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='成功'))
             except psycopg2.errors.UniqueViolation:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='已註冊'))
-            except psycopg2.errors.StringDataRightTruncation:
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='姓名請小於50字'))
             except:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='失敗'))
 
